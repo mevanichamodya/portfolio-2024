715c8e38c27a12d75fee2bdaf4e9b14b8010eb07
mevanichamodya <mevani2001@gmail.com>
2024-02-20 22:52:07 +0530

    portfolio init

---
+++ src/Components/MainPage.js
@@ -0,0 +1,60 @@
+/* eslint-disable jsx-a11y/alt-text */
+function MainPage() {
+    return(
+        <div>
+            <div className="mainPage">
+                <div className="topupBar">
+                    <div className="logoBox">
+                    <img src="C:\Users\User\Downloads\hexagon-letter-m.925x1024.png" alt="Logo"></img>
+                    </div>
+                    <div className="barItem">
+                        <div className="about">
+                            <h5 className="aboutText"> 01. <span className="aboutTextGreen">About</span></h5>
+                        </div>
+                        <div className="about1">
+                        <h5 className="aboutText">02. <span className="aboutTextGreen">Experience</span></h5>
+                        </div>
+                        <div className="about2">
+                        <h5 className="aboutText">03. <span className="aboutTextGreen">Work</span></h5>
+                        </div>
+                        <div className="about3">
+                        <h5 className="aboutText">04. <span className="aboutTextGreen">Contact</span></h5>
+                        </div>
+                        <div className="about4">
+                        <h5 className="buttonText"> Resume</h5>
+                        </div>
+
+                    </div>
+                </div>
+                <div className="mainBox">
+                    <div className="permenantLine"></div>
+                    <div className="secondMainBox">
+                        <div className="firstLine">
+                            <h1 className="firstLineText">Hi,  my name is </h1>
+                        </div>
+                        <div className="secondLine">
+                            <h2 className="secondLineText">Mevani Chamodya.</h2>
+                        </div>
+                        <div className="thirdLine">
+                            <h3 className="thirdLineText">I build thing of the web.</h3>
+
+                        </div>
+                        <div className="fourthLine">
+                            <p className="fourthLineText">As a software engineer with a passion for exceptional digital experiences.<br/> Currently, I'm focused on building user-centric products I also have a keen interest<br/> in UI/UX and I am actively pursuing further studies in the field.</p>
+                        </div>
+                        <div className="fifthLine">
+                            <h6 className="fifthLineText">check out my course!</h6>
+                        </div>
+                     
+                    </div>
+                    <div className="permenantLine"></div>
+                  
+                    
+                </div>
+            </div>
+        </div>
+    );
+
+}
+
+export default MainPage
